422f5170ed09fc78d70d9c347269ab1308799c63
melilove <melissa.salazar13@yahoo.com>
2019-01-28 14:56:34 -0500

    Cambios de html y añadi cambios en el css

--- src/main.js
+++ src/main.js
@@ -7,9 +7,7 @@ let plataforma = '';
 data.forEach((data) => {
 let cartilla = `
       <div class = "card-link">
-      <h4>*Nombre*</h4>
       <label class="name"> ${ data.name }</label>
-      <h4>*Imagen*</h4>
       <img class="image" src="${ data.img }" />
       <h4>*Tipo*</h4>
       <label class="type">${ data.type }</label>
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,6 +1,1 @@
 require('../src/data.js');
-const input = [
-{id: 1, name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', type: ['Grass', 'Poison']},
-{id: 2, name: 'Ivysaur', img: 'http://www.serebii.net/pokemongo/pokemon/002.png', type: ['Grass', 'Poison']},
-{id: 49, name: 'Venomoth', img: 'http://www.serebii.net/pokemongo/pokemon/049.png', type: ['Bug', 'Poison']}
-];
