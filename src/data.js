c65caa7449e7d2fcd0625866967ffdb1733c607e
melilove <melissa.salazar13@yahoo.com>
2019-01-31 11:04:18 -0500

    haciendo el ordenado y filtrado

--- src/data.js
+++ src/data.js
@@ -5,17 +5,18 @@ const mostrarPokemones = (data) => {
   }
   return arrayMostrar;
 };
+//la función es para ordenar alfabeticamente 
 
 const ordenarPorNombre = (data, tipo) => {
   let arrayOrdenar = [];
   for (let i = 0 ; i < data.length ; i++) {
     arrayOrdenar.push({name: data[i].name, img: data[i].img, type: data[i].type});
   }
-  arrayOrdenar.sort((prev, next) => {
-    if (prev.name > next.name) {
+  arrayOrdenar.sort((prevNom, nextNom) => {
+    if (prevNom.name > nextNom.name) {
       return 1;
     }
-    if (prev.name < next.name) {
+    if (prevNom.name < nextNom.name) {
       return -1;
     }
     return 0;
@@ -28,7 +29,37 @@ const ordenarPorNombre = (data, tipo) => {
   }
 };
 
+//filtar por tipo
+
+const filtrarPokemones = (data, tipo) => {
+  let arrayFiltrar = [];
+  arrayFiltrar = data
+    .filter((elemento) => {
+      for (let i = 0 ; i < elemento.type.length ; i++) {
+        if (elemento.type[i] === tipo) {
+          return 1;
+        }
+      }
+    });
+  return arrayFiltrar;
+};
+// filtrar por debilidad y crear la funcion contar
+
+const filtrarPorDebilidad = (data, tipo) => {
+  let arrayFiltrarDeb = [];
+  arrayFiltrarDeb = data.filter((elemento) => {
+    for (let i = 0; i < elemento.weaknesses.length; i++) {
+     if (elemento.weaknesses[i] === tipo) {
+       return 1;
+     } 
+    }
+  });
+  return arrayFiltrarDeb;
+};
+
 window.pokemon = {
   mostrarPokemones,
   ordenarPorNombre,
+  filtrarPokemones,
+  filtrarPorDebilidad,
 };
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 
 const containerList = document.getElementById('container-list');
+
 const ordenPokemon = document.getElementById('orden-pokemon');
+
+const tipoPokemon = document.getElementById('tipo-pokemon');
+
+const debilidadPokemon = document.getElementById('debilidad-pokemon');
 
 const arrayPokemon = POKEMON.pokemon;
 
 const arrayMostrado = pokemon.mostrarPokemones(arrayPokemon);
+
+
+
+
+//muestra todos los pokemones
 
 const crearPlantilla = (data) => {
   let plantilla = '';
@@ -13,7 +23,7 @@ const crearPlantilla = (data) => {
     <div class = "card-link">
     <label class="name">${ data.name }</label>
     <img class="image" src="${ data.img }" />
-    <label class="name" src="${ data.type}" /label>
+    <label class="name">${ data.type}</label>
     </div>
     `;
     plantilla += carta;
@@ -22,8 +32,23 @@ const crearPlantilla = (data) => {
 };
 crearPlantilla(arrayMostrado);
 
+//la funcion para el ordenado alfabeticamente
 
 const ordenarPokemon = () => {
   const arrayOrdenado = pokemon.ordenarPorNombre(arrayPokemon, ordenPokemon.value);
   crearPlantilla(arrayOrdenado);
 };
+
+//filtar por tipo
+
+const  filtrarTipo  = () => {
+    const  arrayFiltrado  =  pokemon.filtrarPokemones (arrayPokemon, tipoPokemon . value );
+    crearPlantilla (arrayFiltrado);
+    };
+
+ // filtar por debilidad de pokemon
+ 
+ const filtrarDebilidad = () => {
+     const arrayFiltrard = pokemon.filtrarDebilidad (arrayPokemon, debilidadPokemon.value);
+     crearPlantilla (arrayFiltrard);
+    };
